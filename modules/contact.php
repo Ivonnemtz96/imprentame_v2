f29f493bb685169f46bbc9ab2806d84778092c1c
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-27 11:33:23 -0700

    contacto semiterminado

--- modules/contact.php
+++ modules/contact.php
@@ -1,19 +1,21 @@
 <div class="bread-crumb-section">
     <div class="container">
         <div class="row justify-content-center align-items-center">
-            <div class="col-lg-8 d-flex justify-content-lg-start justify-content-center flex-column">
-                <h2 class="bread-crumb-title">Booking</h2>
+            <div class="col-lg-8 d-flex justify-content-lg-start
+                        justify-content-center flex-column">
+                <h2 class="bread-crumb-title">Contáctanos</h2>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb gap-3 m-0 p-0">
-                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Booking</li>
+                        <li class="breadcrumb-item"><a href="/">Inicio</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Contáctanos</li>
                     </ol>
                 </nav>
             </div>
             <div class="col-lg-4">
                 <div class="circle-text-common circel-tex-area1">
                     <img src="/assets/images/icons/camera-icon.svg" alt="image">
-                    <p id="CircleTypeText1" class="circle-text">SCROLL * DOWN NOW&nbsp; </p>
+                    <p id="CircleTypeText1" class="circle-text">SCROLL *
+                        DOWN NOW&nbsp; </p>
                 </div>
             </div>
         </div>
@@ -21,63 +23,106 @@
 </div>
 
 
-<div class="calendar-section bg-color2 pt-120 pb-120 mb-44">
+<div class="contact-section bg-color2 pt-120 pb-120 mb-44">
     <div class="container">
-        <div class="row align-items-center justify-content-center">
-            <div class="col-xl-4 col-lg-5 col-md-6 col-sm-10">
+        <div class="row justify-content-center gy-5">
+            <div class="col-xl-5 col-lg-6 col-md-6 col-sm-10">
                 <div class="address-block">
-                    <h4>Address :</h4>
-                    <a href="#">168/170, Ave 01,Old York Drive Rich Mirpur, Dhaka, Bangladesh</a>
+                    <h4>Teléfono :</h4>
+                    <a href="tel:+526241574530">+52 624 157 4530</a>
                 </div>
                 <div class="address-block">
-                    <h4>Phone :</h4>
-                    <a href="tel:+012-3456-789102">+012-3456-789102</a>
+                    <h4>Address :</h4>
+                    <a href="#">Fracc. Country del Mar, San Jose del Cabo, <br> Baja California Sur</a>
                 </div>
                 <div class="address-block">
                     <h4>Email :</h4>
-                    <a
-                        href="https://demo.egenslab.com/cdn-cgi/l/email-protection#b8cbcdc8c8d7caccf8ddc0d9d5c8d4dd96dbd7d5"><span
-                            class="__cf_email__"
-                            data-cfemail="cbb8bebbbba4b9bf8baeb3aaa6bba7aee5a8a4a6">[email&#160;protected]</span></a>
-                    <a href="https://demo.egenslab.com/cdn-cgi/l/email-protection#92fbfcf4fdd2f7eaf3ffe2fef7bcf1fdff"><span
-                            class="__cf_email__"
-                            data-cfemail="e0898e868fa08598818d908c85ce838f8d">[email&#160;protected]</span></a>
+                    <a href="hmailto:hola@imprentame.com">hola@imprentame.com</a>
+                </div>
+                <div class="address-block">
+                    <h4>Social :</h4>
+                    <ul class="social-one justify-content-start mt-3">
+                        <li><img src="/assets/images/icons/instagram.svg" alt="image">
+                            <a href="https://www.instagram.com/">IN</a>
+                        </li>
+                        <li>
+                            <img src="/assets/images/icons/Twitter.svg" alt="image">
+                            <a href="https://www.twitter.com/">TW</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-            <div class="col-xl-8 col-lg-7 col-md-6 col-sm-10">
-                <h3 class="form-title">Book Here</h3>
+            <div class="col-xl-7 col-lg-6 col-md-6 col-sm-10">
+                <h3 class="form-title">Have Any Questions</h3>
                 <form class="contact-me-form">
                     <div class="row">
                         <div class="col-lg-6">
                             <div class="form-inner">
-                                <input type="text" placeholder="Enter your name">
+                                <input type="text" name="nombre" placeholder="Ingrese su nombre">
                             </div>
                         </div>
+
                         <div class="col-lg-6">
                             <div class="form-inner">
-                                <input type="text" placeholder="Enter your email">
+                                <input type="text" name="mail" placeholder="Ingrese su correo">
                             </div>
                         </div>
-                        <div class="col-lg-12">
+
+                        <div class="col-lg-6">
+                            <div class="form-inner">
+                                <input type="text" name="tel" placeholder="Ingrese su teléfono">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <div class="form-inner">
+                                <select name="evento" id=""
+                                    style="border: 1px solid rgba(255, 255, 255, 0.1); padding: 19px 25px; transition: all 0.5s ease; width: 100%; background-color: transparent; color: var(--text-light-one);">
+                                    <option value="Tipo de evento" disabled selected>Tipo de evento</option>
+                                    <option value="Boda">Boda</option>
+                                    <option value="XV Años">XV años</option>
+                                    <option value="Evento Social">Evento social</option>
+                                    <option value="Servicio">Servicio</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
                             <div class="form-inner">
                                 <div class="searchbox-input date-picker-input input__list">
-                                    <input autocomplete="off" placeholder="Date" type="text" name="checkIn"
-                                        id="datepicker" value="" class="claender">
+                                    <input autocomplete="off" placeholder="Fecha de celebración" type="text"
+                                        name="fecha" id="datepicker" value="" class="claender">
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-12">
+
+                        <div class="col-lg-6">
                             <div class="form-inner">
-                                <input type="text" placeholder="Phone">
+                                <input type="text" placeholder="Lugar ceremonial">
                             </div>
                         </div>
+
+                        <div class="col-lg-6">
+                            <div class="form-inner">
+                                <input type="text" placeholder="Número de invitados">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <div class="form-inner">
+                                <label for="">Necesitan servicio de video:</label> <br>
+                                <input style="width: 15%;" type="radio" id="" name="video" value="Sí"><label for="html">Sí</label>
+                                <input style="width: 15%;" type="radio" id="" name="video" value="No"><label for="No">No</label>
+                            </div>
+                        </div>
+
                         <div class="col-lg-12">
                             <div class="form-inner">
-                                <textarea rows="6" placeholder="Write Your message"></textarea>
+                                <textarea rows="6" placeholder="Nota"></textarea>
                             </div>
                         </div>
                         <div class="col-12">
-                            <button type="submit" class="eg-btn btn--primary btn--lg">Send Message</button>
+                            <button type="submit" class="eg-btn btn--primary btn--lg">Enviar</button>
                         </div>
                     </div>
                 </form>
