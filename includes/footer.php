8f04ef30a381864116e08d0a0894f87c09df146c
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-31 15:15:02 -0700

    footer ok

--- includes/footer.php
+++ includes/footer.php
@@ -17,21 +17,15 @@
         <div class="row">
             <div class="col-12 text-center d-flex justify-content-between align-items-center">
                 <div class="copyright">
-                    © 2022 <a href="#">PicsZen</a> is Proudly Powered by
-                    <a href="https://themeforest.net/category/site-templates?term=egenslab" class="egns-lab">Egens
-                        Lab</a>.
+                    © 2023 <a href="#">Imprentame.</a> Diseñado por
+                    <a target="_blank" href="https://bananagroup.mx" class="egns-lab">Banana Group Marketing</a>.
                 </div>
                 <div class="footer-social d-lg-flex d-none">
                     <ul class="social-four">
                         <li>
                             <a href="https://www.instagram.com/"><i class="bx bxl-linkedin"></i></a>
                         </li>
-                        <li>
-                            <a href="https://www.dribbble.com/"><i class="bx bxl-pinterest-alt"></i></a>
-                        </li>
-                        <li>
-                            <a href="https://www.pinterest.com/"><i class="bx bxl-instagram"></i></a>
-                        </li>
+                        
                         <li>
                             <a href="https://www.twitter.com/"><i class="bx bxl-facebook"></i></a>
                         </li>
