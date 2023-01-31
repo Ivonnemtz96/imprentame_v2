37140294600e140db45a96ac61d9b269da7e13b5
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-31 11:01:16 -0700

    fotos de portafolio para bodas en la playa ok

--- modules/detail_beach.php
+++ modules/detail_beach.php
@@ -83,35 +83,35 @@
             </div>
             <div class="row g-4 justify-content-center">
                 <div class="col-lg-6">
-                    <a href="/assets/images/portfolio/port-details-1-big.png" data-fancybox="gallery"
+                    <a href="/assets/images/portfolio/port-details-6-big.webp" data-fancybox="gallery"
                         data-cursor="View Gallery">
-                        <img src="/assets/images/portfolio/port-details-1.png" alt="image">
+                        <img src="/assets/images/portfolio/port-details-6.webp" alt="image">
                     </a>
                 </div>
                 <div class="col-lg-6">
                     <div class="row g-4">
                         <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-                            <a href="/assets/images/portfolio/port-details-2-big.png" data-fancybox="gallery"
+                            <a href="/assets/images/portfolio/port-details-7-big.webp" data-fancybox="gallery"
                                 data-cursor="View Gallery">
-                                <img src="/assets/images/portfolio/port-details-2.png" alt="image">
+                                <img src="/assets/images/portfolio/port-details-7.webp" alt="image">
                             </a>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-                            <a href="/assets/images/portfolio/port-details-3-big.png" data-fancybox="gallery"
+                            <a href="/assets/images/portfolio/port-details-8-big.webp" data-fancybox="gallery"
                                 data-cursor="View Gallery">
-                                <img src="/assets/images/portfolio/port-details-3.png" alt="image">
+                                <img src="/assets/images/portfolio/port-details-8.webp" alt="image">
                             </a>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-                            <a href="/assets/images/portfolio/port-details-4-big.png" data-fancybox="gallery"
+                            <a href="/assets/images/portfolio/port-details-9-big.webp" data-fancybox="gallery"
                                 data-cursor="View Gallery">
-                                <img src="/assets/images/portfolio/port-details-4.png" alt="image">
+                                <img src="/assets/images/portfolio/port-details-9.webp" alt="image">
                             </a>
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-                            <a href="/assets/images/portfolio/port-details-5-big.png" data-fancybox="gallery"
+                            <a href="/assets/images/portfolio/port-details-10-big.webp" data-fancybox="gallery"
                                 data-cursor="View Gallery">
-                                <img src="/assets/images/portfolio/port-details-5.png" alt="image">
+                                <img src="/assets/images/portfolio/port-details-10.webp" alt="image">
                             </a>
                         </div>
                     </div>
