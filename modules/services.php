1d9f1cf1afc62db77add552aff4725bcd078f2b4
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-31 12:13:03 -0700

    Servicios ok

--- modules/services.php
+++ modules/services.php
@@ -2,11 +2,11 @@
     <div class="container">
         <div class="row justify-content-center align-items-center">
             <div class="col-lg-8 d-flex justify-content-lg-start justify-content-center flex-column">
-                <h2 class="bread-crumb-title">Services</h2>
+                <h2 class="bread-crumb-title">Servicios</h2>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb gap-3 m-0 p-0">
-                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Services</li>
+                        <li class="breadcrumb-item"><a href="/">Inicio</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Más servicios</li>
                     </ol>
                 </nav>
             </div>
@@ -27,60 +27,66 @@
             <div class="col-lg-6 col-md-6 col-sm-6">
                 <div class="service-single">
                     <div class="service-image">
-                        <img src="/assets/images/bg/service-1.png" alt="image">
+                        <img src="/assets/images/bg/service-1.webp" alt="image">
                     </div>
                     <div class="service-content">
                         <img src="/assets/images/icons/service-icon1.svg" alt="image">
-                        <h3><a href="#">Personal</a></h3>
-                        <p>My name is Johnal Dainok. I live in Ohiogi USA. I’ve been ar photographer for overd 15 years
-                            now. I started getting one top of thisant for this is our variental group intro gonateon
-                            photography when my moved.</p>
-                        <a class="eg-btn btn--primary btn--lg" href="#">Appointment Now</a>
+                        <h3><a href="#">Restauraciones</a></h3>
+                        <p>
+                            El retoque fotográfico puede ser sumamente minucioso, el dominar el pincel digital, jugar
+                            con las luces y sombras, dar color por diferentes zonas de la imagen, agregar brillo y
+                            textura en la piel, dominar los complejos programas de edición fotografía, requiere un
+                            amplio conocimiento, práctica y actualización constante.
+                        </p>
+                        <a class="eg-btn btn--primary btn--lg" href="#">Contactar</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-6">
                 <div class="service-single">
                     <div class="service-image">
-                        <img src="/assets/images/bg/service-2.png" alt="image">
+                        <img src="/assets/images/bg/service-3.webp" alt="image">
                     </div>
                     <div class="service-content">
                         <img src="/assets/images/icons/service-icon2.svg" alt="image">
-                        <h3><a href="#">Wedding</a></h3>
-                        <p>My name is Johnal Dainok. I live in Ohiogi USA. I’ve been ar photographer for overd 15 years
-                            now. I started getting one top of thisant for this is our variental group intro gonateon
-                            photography when my moved.</p>
-                        <a class="eg-btn btn--primary btn--lg" href="#">Appointment Now</a>
+                        <h3><a href="#">Bebés</a></h3>
+                        <p>
+                            El fondo en cada foto y/o video importa mucho. Realzará la belleza y transmite magia en cada
+                            fotografía y/o escena. Ese lugar puede muy importante en una sesión, por nos coordinaremos
+                            previamente para elegir en conjunto contigo, el lugar perfecto para tu sesión
+                        </p>
+                        <a class="eg-btn btn--primary btn--lg" href="#">Contactar</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-6">
                 <div class="service-single">
                     <div class="service-image">
-                        <img src="/assets/images/bg/service-3.png" alt="image">
+                        <img src="/assets/images/bg/service-4.webp" alt="image">
                     </div>
                     <div class="service-content">
                         <img src="/assets/images/icons/service-icon3.svg" alt="image">
-                        <h3><a href="#">Modeling</a></h3>
-                        <p>My name is Johnal Dainok. I live in Ohiogi USA. I’ve been ar photographer for overd 15 years
-                            now. I started getting one top of thisant for this is our variental group intro gonateon
-                            photography when my moved.</p>
-                        <a class="eg-btn btn--primary btn--lg" href="#">Appointment Now</a>
+                        <h3><a href="#">Productos</a></h3>
+                        <p>
+                        Utilizamos las últimas versiones de software de edición, como son Photoshop, Lightroom, Premier, After Effects. Manejamos cámaras y lentes de la marca Nikon.
+                        </p>
+                        <a class="eg-btn btn--primary btn--lg" href="#">Contactar</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-6">
                 <div class="service-single">
                     <div class="service-image">
-                        <img src="/assets/images/bg/service-4.png" alt="image">
+                        <img src="/assets/images/bg/service-2.webp" alt="image">
                     </div>
                     <div class="service-content">
                         <img src="/assets/images/icons/service-icon4.svg" alt="image">
-                        <h3><a href="#">Editing</a></h3>
-                        <p>My name is Johnal Dainok. I live in Ohiogi USA. I’ve been ar photographer for overd 15 years
-                            now. I started getting one top of thisant for this is our variental group intro gonateon
-                            photography when my moved.</p>
-                        <a class="eg-btn btn--primary btn--lg" href="#">Appointment Now</a>
+                        <h3><a href="#">Embarazos</a></h3>
+                        <p>
+                        Hay ciertos momentos en la vida que quieres hacer inmortales. Quieres vivir en esos momentos para siempre. Tu embarazoa es uno de los momentos especiales.Conmigo obtendrás una ganancia de las fotografías y videos que guardaran los momentos y cuando mira hacia atrás, viajaras con tu mente a esos recuerdos memorables.
+
+                        </p>
+                        <a class="eg-btn btn--primary btn--lg" href="#">Contactar</a>
                     </div>
                 </div>
             </div>
@@ -95,7 +101,7 @@
         <div class="row gy-5 justify-content-center text-center">
             <div class="col-md-10">
 
-                <a class="eg-btn btn--primary btn--lg" href="booking.html">Appointment Now</a>
+                <a class="eg-btn btn--primary btn--lg" href="booking.html">Contactar</a>
 
             </div>
         </div>
