1276a88cb69e2deb5958303c728f3efa9c2045c3
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-05-24 17:02:49 -0700

    Se cambia detalles de bodas como el about sin comentarios

--- modules/detail_wedding.php
+++ modules/detail_wedding.php
@@ -1,162 +1,135 @@
-<div class="bread-crumb-section">
+<div class="about-us-section pt-120 pb-120" data-scroll data-scroll-speed="3">
     <div class="container">
-        <div class="row justify-content-center align-items-center">
-            <div class="col-lg-8 d-flex justify-content-lg-start justify-content-center flex-column">
-                <h2 class="bread-crumb-title">Portafolio bodas religiosas</h2>
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb gap-3 m-0 p-0">
-                        <li class="breadcrumb-item"><a href="/">Inicio</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Portafolio</li>
-                    </ol>
-                </nav>
+        <div class="row justify-content-center align-items-center mb-40
+                    g-4">
+            <div class="col-lg-8 text-lg-start text-center">
+                <div class="about-us-content">
+                    <h2>¿Qué es Imprentame?</h2>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <div class="circle-text-common circel-tex-area1">
-                    <img src="/assets/images/icons/camera-icon.svg" alt="image">
-                    <p id="CircleTypeText1" class="circle-text">SCROLL * DOWN NOW&nbsp; </p>
+            <!-- <div class="col-lg-4 text-lg-end text-center">
+                <a class="eg-btn btn--primary btn--lg" href="/gallery">Ver galería</a>
+            </div> -->
+        </div>
+        <div class="row align-items-center g-4">
+            <div class="col-lg-6">
+                <div class="about-us-image">
+                    <img src="/assets/images/bg/about-us1.webp" alt="image">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="about-us-content">
+                    <p><strong>Imprentame</strong> te ofrece Arte, Fotogratla y Video Profesional en Los Cabos,
+                        ofrecemos diferentes
+                        tipos de
+                        fotogratía, como son: fotografía de boda, fotografia de quinceañera, fotografía de grupos,
+                        fotografía de retrato,
+                        fotografía de embarazo, fotografía de reclén nacido, fotografia de niños, fotografia familiar,
+                        fotografla de
+                        producto, fotografia de alimentos Y bebidas, fotografia de overa, fotografla de moda, fotografia
+                        de tours
+                        privados, fotografia de graduaciones, entre otras.</p>
+
+                    <p>
+                        Utilizamos las últimas versiones de software de edición, como son Photoshop, Lightroom, Premier,
+                        After Effects. Manejamos cámaras y lentes de la marca Nikon. Hablamos inglés y español.
+                    </p>
+                    <p>
+
+                        Creamos arte, fabricamos finos marcos de madera, en color nogal, manejamos diferentes tamaños,
+                        podemos enmarcar y retocar tu foto favorita de la sesión con un costo adicional. También
+                        contamos con servicios de restauración de fotografías y retoque digital con pincel. Recuerda que
+                        puedes enviarnos tus fotografías tomadas por otro fotógrafo y nosotros las retocamos,
+                        imprimimos, texturizamos y enmarcamos.
+
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="experience-block">
+                    <h2>Experiencia</h2>
+                    <p>
+                        Mi nombre es Jorge Montoya, hablo inglés y español, artista retocador digital, fotógrafo y
+                        videógrafo quien siempre está en
+                        búsqueda de actualizaciones de fotografía, para entregar fotografías creativas y diferentes,
+                        además, especializado en retoque fotográfico y restauración de fotografías con pincel
+                        digital, lo cual garantiza que tus fotografías serán increíbles.
+                    </p>
+                    <ul class="skills-bar-container">
+                        <li>
+                            <div class="progressbar-title">
+                                <h6>Fotografía</h6>
+                                <span class="percent" id="html-pourcent"></span>
+                            </div>
+                            <div class="bar-container">
+                                <span class="progressbar" id="progress-html"></span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="progressbar-title">
+                                <h6>Video</h6>
+                                <span class="percent" id="css-pourcent"></span>
+                            </div>
+                            <div class="bar-container">
+                                <span class="progressbar" id="progress-css"></span>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="progressbar-title">
+                                <h6>Retoques digitales</h6>
+                                <span class="percent" id="javascript-pourcent"></span>
+                            </div>
+                            <div class="bar-container">
+                                <span class="progressbar" id="progress-javascript"></span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="about-image-group d-flex flex-row
+                            flex-nowrap justify-content-lg-start
+                            justify-content-center">
+                    <img src="/assets/images/bg/about-us2.webp" alt="image">
+                    <img src="/assets/images/bg/about-us3.webp" class="d-xxl-block d-none" alt="image">
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-
-<div class="portfolio-detaisl-section bg-color2">
-    <div class="container-fluid">
-        <div class="portfolio-details-image mb-60">
-            <img src="/assets/images/portfolio/boda_religiosa.webp" alt="image">
-        </div>
-    </div>
+<div class="counter-section bg-color1" style="background-color: #808080;">
     <div class="container">
-        <div class="portfolio-details-content">
-            <div class="row gy-5">
-                <div class="col-lg-8">
-                    <div class="photographer-details">
-                        <span>Fotografía</span>
-                        <h2>Capturando momentos increíbles</h2>
-                        <p>
-                            Mi objetivo es hacer que tu día sea más brillante, maravilloso y memorable. Cualquiera puede
-                            tomar fotografías y grabar videos, pero no todos tienen la capacidad de agregarle algo de
-                            magia. Se trata de las habilidades que domino y me apasionan. Me encanta hacer que el día de
-                            tu boda, sea más encantador y deslumbrante, esa es mi primera prioridad.
-                        </p>
-                        <p>
-                            Haré que los recuerdos de tu gran día se vean más vivos e impresionantes. Realizaré
-                            fotografía artística durante el día de la boda y me aseguraré de no perder ni un solo
-                            momento de alguna de tus bellas emociones. Seré extremadamente detallista en la sesión que
-                            me has confiado. Tengo equipo profesional y una gran experiencia y técnica de edición en el
-                            proceso de postproducción, que hará que sea capaz de capturar y entregar fotos perfectas de
-                            tu boda.
-                        </p>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="photo-info-card">
-                        <ul>
-                            <!-- <li>
-                                <h5>Client :</h5>
-                                <span>Johan Luke Julian</span>
-                            </li> -->
-                            <li>
-                                <h5>Categoría :</h5>
-                                <span>Fotografía </span>
-                            </li>
-                            <li>
-                                <h5>Tipo :</h5>
-                                <span>Bodas Religiosas</span>
-                            </li>
-                            <li>
-                                <h5>Dirección :</h5>
-                                <span>San José del Cabo</span>
-                            </li>
-                        </ul>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-6">
+                <div class="counter-single style-two">
+                    <div class="coundown d-flex flex-column">
+                        <h2 class="odometer" data-odometer-final="50">01</h2>
+                        <p class="mb-0">Sesiones Fotográficas</p>
                     </div>
                 </div>
             </div>
-            <div class="row g-4 justify-content-center">
-                <div class="col-lg-6">
-                    <a href="/assets/images/portfolio/port-details-1-big.webp" data-fancybox="gallery"
-                        data-cursor="View Gallery">
-                        <img src="/assets/images/portfolio/port-details-1.webp" alt="image">
-                    </a>
-                </div>
-                <div class="col-lg-6">
-                    <div class="row g-4">
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-                            <a href="/assets/images/portfolio/port-details-2-big.webp" data-fancybox="gallery"
-                                data-cursor="View Gallery">
-                                <img src="/assets/images/portfolio/port-details-2.webp" alt="image">
-                            </a>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-                            <a href="/assets/images/portfolio/port-details-3-big.webp" data-fancybox="gallery"
-                                data-cursor="View Gallery">
-                                <img src="/assets/images/portfolio/port-details-3.webp" alt="image">
-                            </a>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-                            <a href="/assets/images/portfolio/port-details-4-big.webp" data-fancybox="gallery"
-                                data-cursor="View Gallery">
-                                <img src="/assets/images/portfolio/port-details-4.webp" alt="image">
-                            </a>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-                            <a href="/assets/images/portfolio/port-details-5-big.webp" data-fancybox="gallery"
-                                data-cursor="View Gallery">
-                                <img src="/assets/images/portfolio/port-details-5.webp" alt="image">
-                            </a>
-                        </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-6">
+                <div class="counter-single style-two">
+                    <div class="coundown d-flex flex-column">
+                        <h2 class="odometer" data-odometer-final="30">01</h2>
+                        <p class="mb-0">Retoques digitales</p>
                     </div>
                 </div>
             </div>
-            <!-- <div class="overview-text">
-                <h2>Overview</h2>
-                <p>I wanted to capture everything that seemed magical to the eye. After 5 years of thisa andoi
-                    freelancing and working for big companies I founded my own photography agency. I meani back and
-                    forth from Ohio to Nevada for work. I started getting into photography when my family moved from
-                    Nevada to Ohio. Onar Switching places from such a young age made it possible for me to see many
-                    different.</p>
-            </div> -->
-        </div>
-
-        <div class="row justify-content-center text-center mt-5">
-            <div class="col-md-12">
-                <div class="container">
-                    <a class="eg-btn btn--primary btn--lg"
-                        href="https://wa.me/+526241574530?text=Hola,%20me%20gustó%20su%20sitio%20web%20me%20gustaría%20más%20información.">Contactar</a>
-                </div>
-            </div>
-
-        </div>
-    </div>
-    <div class="container-fluid pt-120 mb-44">
-        <div class="portfolio-navigation">
-            <div class="row align-items-center">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-6">
-                    <div class="arrow-prev text-end">
-                        <a href="/detail_xv">
-                            <span>Anterior</span>
-                            <svg width="171" height="10" viewBox="0 0 171 10" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1 5L171 4.99999" />
-                                <path d="M6 1L1 5L6 9" stroke-linecap="round" />
-                            </svg>
-                        </a>
-
+            <div class="col-lg-3 col-md-6 col-sm-6 col-6">
+                <div class="counter-single style-two">
+                    <div class="coundown d-flex flex-column">
+                        <h2 class="odometer" data-odometer-final="30">01</h2>
+                        <p class="mb-0">Bodas</p>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-4 d-sm-block d-none text-center">
-                    <h4>Portafolio</h4>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-6">
-                    <div class="arrow-prev text-start">
-
-                        <a href="/detail_beach">
-                            <span>Siguiente</span>
-                            <svg width="171" height="10" viewBox="0 0 171 10" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M0 5L170 4.99999" />
-                                <path d="M165 9L170 5L165 1" stroke-linecap="round" />
-                            </svg>
-                        </a>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-6">
+                <div class="counter-single style-two">
+                    <div class="coundown d-flex flex-column">
+                        <h2 class="odometer" data-odometer-final="20">01</h2>
+                        <p class="mb-0">XV Años</p>
                     </div>
                 </div>
             </div>
