c26887b759f9bd83fa9aba288fbd1c913dfa9366
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-05-30 12:32:50 -0700

    Sección de videos queda listo, videos ya ni se encuentran

--- modules/video_gallery.php
+++ modules/video_gallery.php
@@ -10,18 +10,12 @@
                     </ol>
                 </nav>
             </div>
-            <div class="col-lg-4">
-                <div class="circle-text-common circel-tex-area1">
-                    <img src="/assets/images/icons/camera-icon.svg" alt="image">
-                    <p id="CircleTypeText1" class="circle-text">SCROLL * DOWN NOW&nbsp; </p>
-                </div>
-            </div>
         </div>
     </div>
 </div>
 
 
-<div class="portfolio-detaisl-section bg-color2">
+<div class="portfolio-detaisl-section">
     <div class="container-fluid">
         <div class="portfolio-details-image mb-60">
             <img src="/assets/images/portfolio/boda_religiosa.webp" alt="image">
@@ -30,7 +24,7 @@
     <div class="container">
         <div class="portfolio-details-content">
             <div class="row gy-5">
-                <div class="col-lg-8">
+                <div class="col-lg-12 mb-5">
                     <div class="photographer-details">
                         <span>Video</span>
                         <h2>Capturando momentos increíbles</h2>
@@ -48,28 +42,6 @@
                         </p>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="photo-info-card">
-                        <ul>
-                            <!-- <li>
-                                <h5>Client :</h5>
-                                <span>Johan Luke Julian</span>
-                            </li> -->
-                            <li>
-                                <h5>Categoría :</h5>
-                                <span>Video </span>
-                            </li>
-                            <li>
-                                <h5>Tipo :</h5>
-                                <span>Videos de diferentes categorías</span>
-                            </li>
-                            <li>
-                                <h5>Dirección :</h5>
-                                <span>San José del Cabo</span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
             </div>
             <div class="row g-4 justify-content-center">
                 <div class="col-lg-6">
@@ -83,14 +55,12 @@
                 <div class="col-lg-6">
                     <div class="row g-4">
                         <div class="col-lg-6 col-md-6 col-sm-6 col-6">
-
                             <iframe
                                 src="https://www.facebook.com/plugins/video.php?height=295&href=https%3A%2F%2Fwww.facebook.com%2Fimprentame%2Fvideos%2F424355139816295%2F&show_text=false&width=560&t=0"
                                 width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no"
                                 frameborder="0" allowfullscreen="true"
                                 allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
                                 allowFullScreen="true"></iframe>
-
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-6">
                             <iframe
@@ -128,46 +98,11 @@
                     different.</p>
             </div> -->
         </div>
-
-        <div class="row justify-content-center text-center mt-5">
+        <div class="row justify-content-center text-center mt-5" style="margin-bottom:7rem;">
             <div class="col-md-12">
                 <div class="container">
                     <a class="eg-btn btn--primary btn--lg"
                         href="https://wa.me/+526241574530?text=Hola,%20me%20gustó%20su%20sitio%20web%20me%20gustaría%20más%20información.">Contactar</a>
-                </div>
-            </div>
-
-        </div>
-    </div>
-    <div class="container-fluid pt-120 mb-44">
-        <div class="portfolio-navigation">
-            <div class="row align-items-center">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-6">
-                    <div class="arrow-prev text-end">
-                        <a href="/marcos">
-                            <span>Anterior</span>
-                            <svg width="171" height="10" viewBox="0 0 171 10" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1 5L171 4.99999" />
-                                <path d="M6 1L1 5L6 9" stroke-linecap="round" />
-                            </svg>
-                        </a>
-
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-4 d-sm-block d-none text-center">
-                    <h4>Portafolio</h4>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-6">
-                    <div class="arrow-prev text-start">
-
-                        <a href="/restauracion">
-                            <span>Siguiente</span>
-                            <svg width="171" height="10" viewBox="0 0 171 10" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M0 5L170 4.99999" />
-                                <path d="M165 9L170 5L165 1" stroke-linecap="round" />
-                            </svg>
-                        </a>
-                    </div>
                 </div>
             </div>
         </div>
